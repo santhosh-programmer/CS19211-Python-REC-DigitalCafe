c742f4ca48da9a05f7b18ebb5613a1cfad27b25b
santhosh <105176741+santhosh-p-official@users.noreply.github.com>
2022-06-08 10:58:43 +0530

    Create Area_of_a_Field.py

---
+++ week-2/Area_of_a_Field.py
@@ -0,0 +1,17 @@
+""" Create a program that reads the length and width of a farmer’s field from the user in feet. Display the area of the field in acres.
+
+Hint: There are 43,560 square feet in an acre.
+
+Sample Input
+
+1000
+
+1000
+
+Sample Output
+
+22.95684113865932 acres"""
+
+a=float(input())
+b=float(input())
+print((a*b)/43560,"acres")
