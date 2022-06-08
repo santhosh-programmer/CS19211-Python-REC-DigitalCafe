7e659d385df7ed3d5c444659dd85ab43c8bcdecf
santhosh <105176741+santhosh-p-official@users.noreply.github.com>
2022-06-08 11:34:59 +0530

    Create Reverse_String.py

---
+++ week-5/Reverse_String.py
@@ -0,0 +1,6 @@
+""" Write a Python program to get one string and reverses a string. The input string is given as an array of characters char[].
+
+You may assume all the characters consist of printable ascii characters."""
+
+a=input()
+print(a[::-1])
