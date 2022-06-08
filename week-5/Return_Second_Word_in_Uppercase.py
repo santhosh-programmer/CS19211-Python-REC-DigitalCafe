bdde7a8150b7cea38fe11160f44c1b3d9f100c77
santhosh <105176741+santhosh-p-official@users.noreply.github.com>
2022-06-08 12:05:43 +0530

    Create Return_Second_Word_in_Uppercase.py

---
+++ week-5/Return_Second_Word_in_Uppercase.py
@@ -0,0 +1,20 @@
+""" Write a program that takes as input a string (sentence), and returns its second word in uppercase.
+
+For example:
+
+If input is “Wipro Technologies Bangalore” the function should return “TECHNOLOGIES”
+
+If input is “Hello World” the function should return “WORLD”
+
+If input is “Hello” the program should return “LESS”
+
+NOTE 1: If input is a sentence with less than 2 words, the program should return the word “LESS”.
+
+NOTE 2: The result should have no leading or trailing spaces."""
+
+a=input()
+b=a.split()
+if len(b)>1:
+    print(b[1].upper())
+else:
+    print("LESS")
