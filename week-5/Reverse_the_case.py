300cf971dc7299ad5fbcaaebbdcff40653f54026
santhosh <105176741+santhosh-p-official@users.noreply.github.com>
2022-06-08 12:06:27 +0530

    Create Reverse_the_case.py

---
+++ week-5/Reverse_the_case.py
@@ -0,0 +1,28 @@
+""" Write a code to reverse the case of a character input
+
+Input Format:
+
+Single character Input
+
+Output Format:
+
+Reversed character
+
+Example Input:
+
+R
+
+Output:
+
+r
+
+Example Input:
+
+a
+
+Output:
+
+A"""
+
+a=input()
+print(a.swapcase())
