a01714bbe596fe5e490c3a784a168bdc6a71eeb9
santhosh <105176741+santhosh-p-official@users.noreply.github.com>
2022-06-11 22:09:45 +0530

    minor error in code...trying to resolve ASAP

--- week-4/assignment/42.py
+++ week-4/assignment/42.py
@@ -1,18 +1,12 @@
 a=int(input())
 #close=0
 #open=1
-close=a
-open=0
 b=[0]*(a)
 for i in range(1,a+1,):
     for j in range(0,a,i):
         if(b[j]==0):
             b[j]=1
-            close-=1
-            open+=1
         else:
             b[j]=0
-            close+=1
-            open-=1
-print("open =",open)
-print("close =",close)
+print("open =",b.count(1))
+print("close =",b.count(0))
