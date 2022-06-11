f678204ed63664b86d0ba1d8a33876290519b235
santhosh <105176741+santhosh-p-official@users.noreply.github.com>
2022-06-11 23:16:47 +0530

    Update 42.py

--- week-4/assignment/42.py
+++ week-4/assignment/42.py
@@ -1,12 +1,8 @@
+import math
 a=int(input())
-#close=0
-#open=1
-b=[0]*(a)
-for i in range(1,a+1):
-    for j in range(0,a,i):
-        if(b[j]==0):
-            b[j]=1
-        else:
-            b[j]=0
-print("open =",b.count(1))
-print("close =",b.count(0))
+open=0
+for i in range(1,math.floor(math.sqrt(a)+1)):
+    open+=1
+print("open =",open)
+print("close =",a-open)
+
