5bffcf1994460d332f7bb42edef0d1065c424069
santhosh <105176741+santhosh-p-official@users.noreply.github.com>
2022-06-11 20:35:30 +0530

    Create 13.py

---
+++ week-4/assignment/13.py
@@ -0,0 +1,2 @@
+a=int(input())
+print("Total Parcels is {:.0f},".format(a),"1st Location {:.0f} parcels,".format(a*0.2),"2nd Location {:.0f} parcels,".format(a*0.4),"3rd Location {:.0f} parcels,".format(a*0.3),"4th Location {:.0f} parcels.".format(a*0.1))
