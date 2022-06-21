2d6094f91d0da08bf92ed72ef463874e25b6a3aa
santhosh <105176741+santhosh-p-official@users.noreply.github.com>
2022-06-21 09:21:13 +0530

    Create 4.py

---
+++ week-7/4.py
@@ -0,0 +1,21 @@
+# Write a python program to read a string and a character, print the number of occurrence of the character in the string and the location of the first occurrence.
+
+# Note: To convert an input string to tuple use tuple(variablename).
+
+# Sample Input
+
+# Apple
+
+# p
+
+# Sample Output
+
+# 2
+
+# 1
+
+
+a=input()
+b=input()
+print(a.count(b))
+print(a.index(b))
