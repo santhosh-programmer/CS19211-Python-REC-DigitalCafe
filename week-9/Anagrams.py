7f70ab5bb92f8052f04f3b3290a008f9a8c1709d
santhosh <105176741+santhosh-p-official@users.noreply.github.com>
2022-06-21 11:05:22 +0530

    Create Anagrams.py

---
+++ week-9/Anagrams.py
@@ -0,0 +1,42 @@
+# Two words are anagrams if they contain all of the same letters, but in a different order. For example, “evil” and “live” are anagrams because each contains one “e”, one “i”, one “l”, and one “v”. Create a program that reads two strings from the user, determines whether or not they are anagrams, and reports the result.
+
+# Sample Input 1
+
+# evil 
+
+# live
+
+# Sample Output 1
+
+# Those strings are anagrams.
+
+# Sample Input 2
+
+# meet 
+
+# met
+
+# Sample Output 2
+
+# Those strings are not anagrams.
+
+
+a=input()
+b=input()
+d1={}
+d2={}
+for i in a:
+    if i not in d1.keys():
+        d1[i]=1
+    else:
+        d1[i]+=1
+for i in b:
+    if i not in d2.keys():
+        d2[i]=1
+    else:
+        d2[i]+=1
+if d1!=d2:
+    print("Those strings are not anagrams.")
+else:
+    print("Those strings are anagrams.")
+        
